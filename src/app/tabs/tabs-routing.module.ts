04d4a7a367f6c6d877fddf7aec6265d444d0db8e
René Cortez <rene.cortes@outlook.com>
2020-02-28 23:03:39 -0600

    Removed unused tab3 files and references.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -28,16 +28,6 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -55,4 +45,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
