ad56b0b5a89d87a245473dbdd1481b63d774c22a
René Cortez <rene.cortes@outlook.com>
2020-03-01 23:14:10 -0600

    import WeatherImageComponentModule for use in forecast-tab component

--- src/app/forecast-tab/forecast-tab.module.ts
+++ src/app/forecast-tab/forecast-tab.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ForecastTabPage } from './forecast-tab.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { WeatherImageComponentModule } from '../weather-image/weather-image.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
+    WeatherImageComponentModule,
     RouterModule.forChild([{ path: '', component: ForecastTabPage }])
   ],
   declarations: [ForecastTabPage]
