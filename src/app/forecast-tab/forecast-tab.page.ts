1a563d72c037937439a32099ff06c45d4e6d67dd
René Cortez <rene.cortes@outlook.com>
2020-03-01 22:09:49 -0600

    Rename "tab2" files and references by "forecast-tab"

---
+++ src/app/forecast-tab/forecast-tab.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: 'forecast-tab.page.html',
+  styleUrls: ['forecast-tab.page.scss']
+})
+export class ForecastTabPage {
+
+  constructor() { }
+
+}
