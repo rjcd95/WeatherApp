430a966c73635830219660656075d0c4f24ff121
René Cortez <rene.cortes@outlook.com>
2020-03-01 23:11:01 -0600

    Created models and get forest data using api service.

--- src/app/forecast-tab/forecast-tab.page.ts
+++ src/app/forecast-tab/forecast-tab.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { ForeCastResponse } from '../model/forecast-response';
+import { ForeCastData } from '../model/forecast-data';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-forecast',
@@ -7,6 +11,29 @@ import { Component } from '@angular/core';
 })
 export class ForecastTabPage {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
+  data: Array<ForeCastData> = [];
+  ngOnInit() {
+    this.getForeCast();
+  }
+
+  getForeCast() {
+    const currentLocation = 'Managua,NI';
+    this.apiService.getForeCast(currentLocation)
+      .subscribe((response: ForeCastResponse) => {
+        let dataResponse = response.list;
+        console.log('dataResponse', dataResponse);
+        dataResponse.forEach(item => {
+          let forecast = {
+            weather_main: item.weather[0].main,
+            weather_description: item.weather[0].description,
+            date: moment(item.dt_txt, 'YYYY-MM-DD hh:mm:ss').format('ddd, MMM DD, YYYY'),
+            temp_min: item.main.temp_min,
+            temp_max: item.main.temp_max
+          }
+          this.data.push(forecast);
+        })
+      });
+  }
 }
